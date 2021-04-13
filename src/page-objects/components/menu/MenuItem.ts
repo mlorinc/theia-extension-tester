b8761ab538c0c931abf67b102cb0149decb703a9
Marian Lorinc <mlorinc@redhat.com>
2021-04-13 16:32:57 +0200

    Add Menu and MenuItem component
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

---
+++ src/page-objects/components/menu/MenuItem.ts
@@ -0,0 +1,33 @@
+import { IMenu, IMenuItem } from "extension-tester-page-objects";
+import { WebElement } from "selenium-webdriver";
+import { TheiaElement } from "../../theia-components/TheiaElement";
+
+export abstract class MenuItem extends TheiaElement implements IMenuItem {
+    constructor(element: WebElement, parent: IMenu) {
+        super(element, parent);
+    }
+
+    abstract getNextMenu(): Promise<IMenu>;
+
+    async select(): Promise<IMenu | undefined> {
+        const nesting = await this.isNesting();
+        await this.safeClick();
+        if (nesting) {
+            return await this.getNextMenu();
+        }
+        else {
+            return undefined;
+        }
+    }
+
+    getParent(): IMenu {
+        return this.enclosingItem as IMenu;
+    }
+
+    async getLabel(): Promise<string> {
+        const element = await this.findElement(TheiaElement.locators.components.menu.label);
+        return element.getText();
+    }
+
+    abstract isNesting(): Promise<boolean>;
+}
