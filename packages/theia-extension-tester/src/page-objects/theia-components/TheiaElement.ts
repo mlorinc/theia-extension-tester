9786ed51ff6004f84c57523a18258b299a164a0b
Marian Lorinc <mlorinc@redhat.com>
2021-04-13 16:41:42 +0200

    Add helper functions for element properties
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- packages/theia-extension-tester/src/page-objects/theia-components/TheiaElement.ts
+++ packages/theia-extension-tester/src/page-objects/theia-components/TheiaElement.ts
@@ -1,58 +1,48 @@
-import { AbstractElement, SeleniumBrowser } from 'extension-tester-page-objects';
+import { AbstractElement, getTimeout, SeleniumBrowser } from 'extension-tester-page-objects';
 import {
     Locator,
     WebDriver,
     WebElement,
     WebElementPromise
 } from 'selenium-webdriver';
-import { isTheiaLocator, TheiaLocator, TheiaLocators } from '../../locators/TheiaLocators';
+import { isTheiaLocator, TheiaLocator, TheiaLocatorProperties, TheiaLocatorProperty, TheiaLocators } from '../../locators/TheiaLocators';
 
 export class TheiaElement extends AbstractElement {
     protected static _locators: TheiaLocators;
     private _theiaLocator?: TheiaLocator;
+    protected properties?: TheiaLocatorProperties;
 
-    constructor(base: Locator | WebElement | TheiaLocator, enclosingItem?: WebElement | Locator | TheiaLocator) {
+    constructor(base: Locator | WebElement | TheiaLocator, enclosingItem?: Locator | WebElement | TheiaLocator, baseTheiaLocator?: TheiaLocator) {
         const baseLocator = TheiaElement.handleLocator(base);
-        const parentLocator = enclosingItem ? TheiaElement.handleLocator(enclosingItem) : enclosingItem;
-        super(baseLocator, parentLocator);
+        const parentLocator = TheiaElement.handleLocator(enclosingItem);
 
-        if (isTheiaLocator(base)) {
+        if (baseLocator !== undefined) {
+            super(baseLocator, parentLocator);
+        }
+        else {
+            throw new Error('Base locator must not be undefined.');
+        }
+
+        if (base instanceof TheiaElement) {
+            this._theiaLocator = base.theiaLocator;
+        }
+        else if (baseTheiaLocator) {
+            this._theiaLocator = baseTheiaLocator;
+        }
+        else if (isTheiaLocator(base)) {
             this._theiaLocator = base as TheiaLocator;
         }
     }
 
-    get theiaLocator() : TheiaLocator | undefined {
-        return this._theiaLocator; 
-    }
-    
-    async click(timeout?: number): Promise<void> {
-        return super.click(timeout || await SeleniumBrowser.instance.getImplicitTimeout());
-    }
-
-    async waitInteractive(timeout?: number): Promise<this> {
-        try {
-            return super.waitInteractive(timeout || await SeleniumBrowser.instance.getImplicitTimeout());
-        }
-        catch (e) {
-            if (this.theiaLocator) {
-                const locator = this.theiaLocator.locator.toString();
-                const clazz = this.theiaLocator.class?.constructor || 'Unknown';
-                const dependencyLocator = this.theiaLocator.dependency?.call(undefined, TheiaElement.locators);
-                const dependsOn = dependencyLocator?.class?.constructor || dependencyLocator?.locator.toString() || 'Unknown';
-
-                throw new Error(`${e}\nLocator: ${locator}, Class: ${clazz}, Depends on: ${dependsOn}`);
-            }
-            else {
-                throw e;
-            }
-        }
+    get theiaLocator(): TheiaLocator | undefined {
+        return this._theiaLocator;
     }
 
     static get locators(): TheiaLocators {
         return this._locators;
     }
 
-    private static handleLocator(base: WebElement | Locator | TheiaLocator): Locator | WebElement {
+    private static handleLocator(base?: WebElement | Locator | TheiaLocator): Locator | WebElement | undefined {
         if (isTheiaLocator(base)) {
             const theiaLocator = base as TheiaLocator;
             return theiaLocator.locator;
@@ -63,17 +53,162 @@ export class TheiaElement extends AbstractElement {
     }
 
     findElement(locator: Locator | TheiaLocator): WebElementPromise {
-        const element = super.findElement(TheiaElement.handleLocator(locator) as Locator)
-            .then((element) => new TheiaElement(element, this));
+        const baseLocator = TheiaElement.handleLocator(locator) as Locator;
+        const loc: TheiaLocator | undefined = isTheiaLocator(locator) ? (locator as TheiaLocator) : undefined;
+        const element = this.getDriver()
+            .wait(
+                () => super.findElement(baseLocator).catch(() => undefined), SeleniumBrowser.instance.findElementTimeout,
+                `Could not find element with locator "${baseLocator.toString()}" relative to "${this.constructor.name}".`
+            )
+            .then((element) => new TheiaElement(element as WebElement, this, loc))
+
         return new WebElementPromise(this.getDriver(), element as Promise<TheiaElement>);
     }
 
     async findElements(locator: Locator | TheiaLocator): Promise<WebElement[]> {
         const elements = await super.findElements(TheiaElement.handleLocator(locator) as Locator);
-        return elements.map((element) => new TheiaElement(element, this));
+        const loc: TheiaLocator | undefined = isTheiaLocator(locator) ? (locator as TheiaLocator) : undefined;
+        return elements.map((element) => new TheiaElement(element, this, loc));
     }
 
+    async focus(): Promise<void> {
+        await this.getDriver().executeScript('arguments[0].focus()', this);
+    }
+
+    async getProperty(property: TheiaLocatorProperty): Promise<string | number | boolean> {
+        if (this.theiaLocator) {
+            if (this.theiaLocator.properties) {
+                const fn = this.theiaLocator.properties[property];
+                if (fn) {
+                    await this.getDriver().wait(() => this.getEnclosingElement(), getTimeout(), 'Waiting for enclosing element to load.');
+                    const value = await fn(this, TheiaElement.locators);
+                    return value;
+                }
+                else {
+                    throw new Error(`${this.constructor.name} does not have defined "${property}" property in locators file.`);
+                }
+            }
+            else {
+                throw new Error(`${this.constructor.name} does not have defined properties in locators file.`);
+            }
+        }
+        else {
+            throw new Error(`Cannot use TheiaElement.getProperty on elements not initialized with TheiaLocator or TheiaElement. Called from ${this.constructor.name}.`);
+        }
+    }
+
+    hasProperty(property: TheiaLocatorProperty): boolean {    
+        if (this.theiaLocator) {
+            return this.theiaLocator.properties !== undefined && this.theiaLocator.properties[property] !== undefined;
+        }
+        else {
+            return false;
+        }
+    }
+
+    async isCollapsed(): Promise<boolean> {
+        if (this.hasProperty('collapsed')) {
+            return await this.getProperty('collapsed') as boolean;
+        }
+        throw new Error(`${this.constructor.name} does not have collapsed property.`);
+    }
+
+    async isDirty(): Promise<boolean> {
+        if (this.hasProperty('dirty')) {
+            return await this.getProperty('dirty') as boolean;
+        }
+        throw new Error(`${this.constructor.name} does not have dirty property.`);
+    }
+
+    async isExpandable(): Promise<boolean> {
+        if (this.hasProperty('expandable')) {
+            return await this.getProperty('expandable') as boolean;
+        }
+        throw new Error(`${this.constructor.name} does not have expandable property.`);
+    }
+
+    async isFocused(): Promise<boolean> {
+        if (this.hasProperty('focused')) {
+            return await this.getProperty('focused') as boolean;
+        }
+        throw new Error(`${this.constructor.name} does not have focused property.`);
+    }
+
+    async getIndex(): Promise<number> {
+        if (this.hasProperty('index')) {
+            return await this.getProperty('index') as number;
+        }
+        throw new Error(`${this.constructor.name} does not have index property.`);
+    }
+
+    async isMain(): Promise<boolean> {
+        if (this.hasProperty('main')) {
+            return await this.getProperty('main') as boolean;
+        }
+        throw new Error(`${this.constructor.name} does not have main property.`);
+    }
+
+    async isSecondary(): Promise<boolean> {
+        if (this.hasProperty('secondary')) {
+            return await this.getProperty('secondary') as boolean;
+        }
+        throw new Error(`${this.constructor.name} does not have secondary property.`);
+    }
+
+    async isSelected(): Promise<boolean> {
+        if (this.hasProperty('selected')) {
+            return await this.getProperty('selected') as boolean;
+        }
+        return await super.isSelected();
+    }
+
+    async getTitle(): Promise<string> {
+        if (this.hasProperty('title')) {
+            return await this.getProperty('title') as string;
+        }
+        throw new Error(`${this.constructor.name} does not have title.`);
+    }
+
+    async isEnabled(): Promise<boolean> {
+        if (this.hasProperty('enabled')) {
+            return await this.getProperty('enabled') as boolean;
+        }
+        else {
+            return await super.isEnabled();
+        }
+    }
+
+    async getComputedStyle(property: string): Promise<unknown> {
+        return await this.getDriver().executeScript(`getComputedStyle(arguments[0]).${property}`, this);
+    }
+
+    async getComputedStyleNumber(property: string): Promise<number> {
+        const value = await this.getComputedStyle(property);
+
+        if (value) {
+            if (typeof value === 'string') {
+                const parsedValue = Number.parseInt(value);
+
+                if (Number.isNaN(parsedValue)) {
+                    console.warn(`Value "${value}" was parsed to NaN.`);
+                    return 0;   
+                }
+                return parsedValue;
+            }
+            else {
+                throw new Error(`Unknown type to parse: ${typeof value}.`);
+            }
+        }
+        else {
+            return 0;
+        }
+    };
+
     static init(locators: TheiaLocators, driver: WebDriver, browser: string, version: string) {
+        if (locators === undefined) {
+            throw new Error('Could not load locators.');
+        }
+
         TheiaElement._locators = locators;
         AbstractElement.driver = driver;
         AbstractElement.versionInfo = { version: version, browser: browser };
