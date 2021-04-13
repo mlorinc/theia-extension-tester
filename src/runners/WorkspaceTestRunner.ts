7353d1fefb54f68155c6a0061e840238c31fee5d
Marian Lorinc <mlorinc@redhat.com>
2021-04-13 16:31:12 +0200

    Make runners more versatile
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/runners/WorkspaceTestRunner.ts
+++ src/runners/WorkspaceTestRunner.ts
@@ -2,11 +2,10 @@ import { DashboardTestRunner } from './DashboardTestRunner';
 import Mocha = require('mocha');
 import { TheiaElement } from '../page-objects/theia-components/TheiaElement';
 import { TabsWidget } from '../page-objects/theia-components/widgets/tabs/TabsWidget';
-import { ListWidget } from '../page-objects/theia-components/widgets/list/ListWidget';
 import { TabWidget } from '../page-objects/theia-components/widgets/tabs/TabWidget';
-import { hasClass } from '../locators/TheiaLocators';
-import { SeleniumBrowser } from 'extension-tester-page-objects';
-import { By } from 'selenium-webdriver';
+import { getTimeout, SeleniumBrowser } from 'extension-tester-page-objects';
+import { By, until } from 'selenium-webdriver';
+import { CheBrowser } from '../browser';
 
 export interface WorkspaceTestRunnerOptions {
     mochaOptions?: Mocha.MochaOptions,
@@ -17,47 +16,54 @@ export interface WorkspaceTestRunnerOptions {
 }
 
 export class WorkspaceTestRunner extends DashboardTestRunner {
-    constructor(browser: SeleniumBrowser, protected options: WorkspaceTestRunnerOptions) {
+    constructor(browser: CheBrowser, protected options: WorkspaceTestRunnerOptions) {
         super(browser, options.mochaOptions);
     }
 
     protected async setup(): Promise<void> {
-        const driver = this.browser.driver;
-        const navbar = await driver.findElement(TheiaElement.locators.dashboard.menu.locator);
+        const timeout = getTimeout(this.browser.pageLoadTimeout) || 30000;
+        const rawNavbar = await this.browser.driver.wait(
+            until.elementLocated(TheiaElement.locators.dashboard.menu.locator), timeout
+        );
 
-        const menu = new ListWidget(navbar);
-        const item = await menu.getListItemByText('Get Started Page');
-        await item.click();
-        
-        const mainSection = await driver.findElement(TheiaElement.locators.dashboard.getStarted.mainSection.locator);
-
-        console.log('Searching tabs');
-        const tabsWidget = new TabsWidget(mainSection);
-        console.log('Found tabs');
-        const getStartedTab: TabWidget = await tabsWidget.getTabByText('Get Started');
-        console.log('Get Started');
-        const customWorkspaceTab: TabWidget = await tabsWidget.getTabByText('Custom Workspace');
-        console.log('Custom Workspace');
+        const navbar = new TheiaElement(rawNavbar);
 
         if (this.options.devfileUrl == null && this.options.devfileTemplate == null) {
             if (this.options.useExistingWorkspace) {
                 const text = this.options.workspaceName.toLowerCase().replace(/\s+/g, '-');
                 console.log(`Looking for workspace: ${text}`);
-                const item = await menu.getListItemByIncludedText(text);
-                console.log(`Found workspace: ${await item.getText()}`);
-                await item.click();
+
+                await this.browser.driver.wait(async () => {
+                    try {
+                        const item = await navbar.findElement(By.xpath(`.//*[contains(text(), '${text}')]`)) as TheiaElement;
+                        console.log(`Found workspace: ${await item.getText()}`);
+                        await item.safeClick();
+                        return true;
+                    }
+                    catch {
+                        return false;
+                    }
+                }, timeout, `Could not click on ${this.options.workspaceName}.`);
             }
             else {
-                await getStartedTab.click();
-                const input = await driver.findElement(By.xpath('.//input[@aria-label="Filter samples list"]'));
-                await input.sendKeys(this.options.workspaceName);
-                const stackCard = await driver.findElement(By.xpath(`.//*[${hasClass('pf-l-gallery')}]//article//*[text()="${this.options.workspaceName}"]`));
-                await stackCard.click();
+                throw new Error('Not supported.');
             }
         } 
         else {
-            await customWorkspaceTab.click();
+            const item = await navbar.findElement(By.xpath(`.//*[contains(text(), 'Get Started')]`)) as TheiaElement;
+            await item.safeClick();
+            
+            const mainSection = new TheiaElement(TheiaElement.locators.dashboard.getStarted.mainSection);
+    
+            console.log('Searching tabs');
+            const tabsWidget = new TabsWidget(mainSection);
+            console.log('Get Started');
+            const customWorkspaceTab: TabWidget = await tabsWidget.getTabByText('Custom Workspace');
+            console.log('Custom Workspace');
+
+            await customWorkspaceTab.safeClick();
         }
+
         await SeleniumBrowser.instance.waitForWorkbench();
     }
 }
