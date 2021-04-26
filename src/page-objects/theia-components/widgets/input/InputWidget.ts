f9bfc5ddf4d618dccf91228ae5a07491f1ddf0cf
Marian Lorinc <mlorinc@redhat.com>
2021-04-26 13:21:34 +0200

    Stabilize input elements
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/page-objects/theia-components/widgets/input/InputWidget.ts
+++ src/page-objects/theia-components/widgets/input/InputWidget.ts
@@ -1,14 +1,15 @@
 import {
-    Button,
-    ExtestUntil,
+    AbstractElement,
     getTimeout,
     Key,
     Locator,
     repeat,
+    SeleniumBrowser,
     TheiaElement,
     TheiaLocator,
     WebElement
 } from '../../../../module';
+import * as clipboardy from "clipboardy";
 
 export interface InputWidgetOptions {
     element?: WebElement | Locator | TheiaLocator;
@@ -19,85 +20,98 @@ export class InputWidget extends TheiaElement {
         super(options?.element || TheiaElement.locators.widgets.input, options?.parent);
     }
 
-    /**
-     * input.getAttribute('value') does not always return actual value. It is caused by invisible input element.
-     * @returns actual value of input
-     */
-    private async getInputValue(): Promise<string> {
-        let value = await this.getDriver().executeScript('arguments[0].value', this);
-
-        if (value === null || value === undefined) {
-            value = '';
-        }
-
-        return value as string;
-    }
-
     async getText(): Promise<string> {
-        return this.getAttribute('value');
+        // can return null
+        return (await this.getAttribute('value')) ?? '';
     }
 
     async setText(text: string, timeout?: number): Promise<void> {
-        await this.safeSetText(text, timeout, 0);
+        await this.safeSetText(text, timeout);
     }
 
-    async safeSetText(text: string, timeout?: number, threshold?: number): Promise<void> {
-        await this.getDriver().wait(ExtestUntil.elementInteractive(this), getTimeout(timeout));
-        await this.safeClick(Button.LEFT, timeout);
-        await this.safeClear(timeout, threshold);
-        await this.getDriver().wait(ExtestUntil.elementInteractive(this), getTimeout(timeout));
-        await this.safeSendKeys(timeout, text);
-        await this.getDriver().wait(
-            async () => await this.getText() === text,
-            timeout,
-            `Timed out setting text to "${text}". Input text: "${await this.getText()}"`
-        );
+    async safeSetText(text: string, timeout?: number): Promise<void> {
+        const oldClipboard = clipboardy.readSync();
+
+        async function waitCondition(this: InputWidget) {
+            return text === await this.getText();
+        }
+
+        try {
+            timeout = getTimeout(timeout);
+            // choose slower approach, multi step input
+            await this.safeClear(timeout, 500);
+            clipboardy.writeSync(text);
+            await this.safeSendKeys(timeout, Key.chord(AbstractElement.ctlKey, 'v'));
+
+            await repeat(waitCondition.bind(this), {
+                timeout: getTimeout(timeout),
+                message: `Could not set text: "${text}".`
+            });
+        }
+        finally {
+            clipboardy.writeSync(oldClipboard);
+        }
     }
 
     async confirm(timeout?: number): Promise<void> {
+        timeout = getTimeout(timeout);
+        await this.focus();
         await this.safeSendKeys(timeout, Key.ENTER);
     }
 
-    async cancel(timeout?: number): Promise<void> {
-        await this.safeClick(Button.LEFT, timeout);
-        await this.safeSendKeys(undefined, Key.ESCAPE);
+    async clear(): Promise<void> {
+        await this.safeClear();
+    }
+
+    async safeClear(timeout?: number, threshold?: number): Promise<void> {
+        timeout = getTimeout(timeout);
+        let scheduler: NodeJS.Timeout;
+
+
+        async function clear(this:InputWidget) {
+            await this.safeSendKeys(timeout || SeleniumBrowser.instance.findElementTimeout,
+                Key.END, Key.chord(Key.SHIFT, Key.HOME, Key.BACK_SPACE));
+            scheduler = setTimeout(clear.bind(this), 1500);
+        }
+
+        await clear.call(this);
+
+        await repeat(async () => {
+            const text = await this.getText();
+            if (text === '') {
+                clearTimeout(scheduler);
+                scheduler = setTimeout(clear.bind(this), 1500);
+                return true;
+            } 
+        }, {
+            timeout,
+            threshold,
+            message: 'Could not clear input on time.'
+        }).finally(() => clearTimeout(scheduler));
     }
 
     async getPlaceHolder(): Promise<string> {
-        return this.getAttribute('placeholder');
+        return (this.getAttribute('placeholder')) ?? '';
     }
 
     async getTitle(): Promise<string> {
-        return this.getAttribute('title');
+        return (await this.getAttribute('title')) ?? '';
     }
 
-    async clear(timeout?: number): Promise<void> {
-        await this.safeClear(timeout, 0);
-    }
-
-    async safeClear(timeout?: number, clearThreshold: number = 1000): Promise<void> {
+    async focus(): Promise<void> {
         await repeat(async () => {
-            await this.getDriver().wait(ExtestUntil.elementInteractive(this), getTimeout(timeout));
-            const value = await this.getInputValue();
-            const text = await this.getText();
-
-            if (text === '' && value === '') {
+            if (await this.isFocused()) {
                 return true;
             }
-            await this.getDriver().actions()
-                .sendKeys(Key.HOME)
-                .keyDown(Key.SHIFT)
-                .sendKeys(Key.END)
-                .keyUp(Key.SHIFT)
-                .sendKeys(Key.BACK_SPACE)
-                .perform();
-
+            await this.safeClick();
             return false;
         }, {
-            id: 'InputWidget.clear',
-            timeout: getTimeout(timeout),
-            threshold: clearThreshold,
-            message: `Could not clear input field: "${await this.getText()}". Value: "${this.getInputValue()}".`
+            timeout: getTimeout(),
+            message: 'Could not focus input widget.'
         });
+    }
+
+    async isFocused(): Promise<boolean> {
+        return true;
     }
 }
