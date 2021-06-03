1eea5a1884457e6b27255d736d61818b744c3b1b
Marian Lorinc <mlorinc@redhat.com>
2021-06-03 18:13:47 +0200

    Use Key.chord function
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/page-objects/components/editor/TextEditor.ts
+++ src/page-objects/components/editor/TextEditor.ts
@@ -31,32 +31,7 @@ export class TextEditor extends Editor implements ITextEditor {
     }
 
     async sendKeys(...var_args: (string | Promise<string>)[]): Promise<void> {
-        let builder = this.getDriver().actions();
-        const controls: { [key: string]: boolean } = {
-            [Key.META]: false,
-            [Key.ALT]: false,
-            [Key.SHIFT]: false,
-            [Key.COMMAND]: false,
-            [Key.CONTROL]: false
-        }
-
-        for (const key of var_args) {
-            const state = controls[await key];
-            if (state !== undefined) {
-                if (state) {
-                    builder = builder.keyUp(await key);
-                }
-                else {
-                    builder = builder.keyDown(await key);
-                }
-                controls[await key] = !controls[await key]
-            }
-            else {
-                builder = builder.sendKeys(key);
-            }
-        }
-
-        await builder.perform();
+        await this.getDriver().actions().sendKeys(...var_args).perform();
     }
 
     async isDirty(): Promise<boolean> {
@@ -65,7 +40,7 @@ export class TextEditor extends Editor implements ITextEditor {
     }
 
     async save(): Promise<void> {
-        await this.sendKeys(AbstractElement.ctlKey, 's', AbstractElement.ctlKey);
+        await this.sendKeys(Key.chord(AbstractElement.ctlKey, 's'));
         await this.getDriver().wait(async () => await this.isDirty() === false, getTimeout(), 'Could not save editor. Dirty flag is visible.');
     }
 
@@ -95,7 +70,7 @@ export class TextEditor extends Editor implements ITextEditor {
     async toggleContentAssist(open: boolean): Promise<void | IContentAssist> {
         if (open) {
             if (await ContentAssist.isOpen(this) === false) {
-                await this.sendKeys(AbstractElement.ctlKey, Key.SPACE, AbstractElement.ctlKey);
+                await this.sendKeys(Key.chord(AbstractElement.ctlKey, Key.SPACE));
             }
 
             const assist = new ContentAssist(this);
@@ -111,7 +86,7 @@ export class TextEditor extends Editor implements ITextEditor {
         const oldClipboard = clipboardy.readSync();
         try {
             clipboardy.writeSync('getText');
-            await this.sendKeys(AbstractElement.ctlKey, 'ac', AbstractElement.ctlKey);
+            await this.sendKeys(Key.chord(AbstractElement.ctlKey, 'ac'));
             await this.getDriver().wait(() => clipboardy.readSync() !== 'getText', getTimeout());
             return clipboardy.readSync();
         }
@@ -131,14 +106,14 @@ export class TextEditor extends Editor implements ITextEditor {
 
     async clearText(): Promise<void> {
         await this.focus();
-        await this.sendKeys(AbstractElement.ctlKey, 'a', AbstractElement.ctlKey, Key.DELETE);
+        await this.sendKeys(Key.chord(AbstractElement.ctlKey, 'a', Key.DELETE));
         await this.getDriver().wait(async () => (await this.getText()).trim().length === 0, getTimeout(), 'Could not clear text.');
     }
 
     async getTextAtLine(line: number): Promise<string> {
         try {
             await this.moveCursor(line, 1);
-            await this.sendKeys(Key.SHIFT, Key.END, Key.SHIFT, AbstractElement.ctlKey, 'c', AbstractElement.ctlKey);
+            await this.sendKeys(Key.chord(Key.SHIFT, Key.END), Key.chord(AbstractElement.ctlKey, 'c'));
             return clipboardy.read();
         }
         finally {
@@ -147,7 +122,7 @@ export class TextEditor extends Editor implements ITextEditor {
     }
     async setTextAtLine(line: number, text: string): Promise<void> {
         await this.moveCursor(line, 1);
-        await this.sendKeys(Key.END, Key.SHIFT, Key.HOME, Key.SHIFT, Key.BACK_SPACE, text);
+        await this.sendKeys(Key.END, Key.chord(Key.SHIFT, Key.HOME), Key.BACK_SPACE, text);
     }
     async typeText(line: number, column: number, text: string): Promise<void> {
         await this.moveCursor(line, column);
@@ -155,7 +130,7 @@ export class TextEditor extends Editor implements ITextEditor {
     }
     async moveCursor(line: number, column: number): Promise<void> {
         await this.focus();
-        await this.sendKeys(AbstractElement.ctlKey, Key.HOME, AbstractElement.ctlKey, ...Array<string>(line - 1).fill(Key.ARROW_DOWN), ...Array<string>(column - 1).fill(Key.ARROW_RIGHT));
+        await this.sendKeys(Key.chord(AbstractElement.ctlKey, Key.HOME), ...Array<string>(line - 1).fill(Key.ARROW_DOWN), ...Array<string>(column - 1).fill(Key.ARROW_RIGHT));
     }
     async getNumberOfLines(): Promise<number> {
         const lines = await this.findElement(TheiaElement.locators.components.editor.lines) as TheiaElement;
